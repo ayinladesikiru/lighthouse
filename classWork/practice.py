45f25fefb272c0f1053ba46c9f7b45b8d2193e6f
ayinlade sikiru <ayinladesikiru@gmail.com>
2024-12-03 11:18:36 +0100

    added a practise file with welcome message

---
+++ classWork/practice.py
@@ -0,0 +1,24 @@
+"""
+print("welcome to python, lighthouse")
+
+number = 1
+
+number = "hawanat"
+
+char = 'M'
+
+print(type(number))
+
+radius = int(input("Enter ya raidus: "))
+
+area = 3.142 * radius ** 2
+
+
+print("the area of circle with radius", radius, "is", area)
+
+"""
+
+name = input("Enter your name: ")
+
+print(len(name))
+
